ad45b97eb2e1dd2da7a00631db90ca3fff713f33
VOOTLA SAI CHANDU <saichanduvootla@gmail.com>
2020-08-27 20:29:12 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -13,16 +13,14 @@ public class Account {
   Scanner input = new Scanner(System.in);
   DecimalFormat moneyFormat =new DecimalFormat("'$'###,##0.00");
 
-public int setCustomerNumber(int customerNumber) {
+public void setCustomerNumber(int customerNumber) {
        this.customerNumber = customerNumber;
- 	return customerNumber;
 }
  	public int getCustomerNumber() {
 		return customerNumber;
 }
-     public int setPinNumber( int pinNumber) {
+     public void setPinNumber( int pinNumber) {
  	this.pinNumber = pinNumber;
- 	return pinNumber;
 }  
 	public int getPinNumber() {
 		return pinNumber;
